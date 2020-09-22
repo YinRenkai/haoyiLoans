fd60d728305b18aad1c7623b67fdb908b49f4fe5
YinRenkai <1246194675@qq.com>
2020-09-22 16:36:53 +0800

    好易贷项目于架构

---
+++ haoyi-service/src/main/java/com/haoyi/service/HaoyiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.haoyi.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaoyiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaoyiServiceApplication.class, args);
+    }
+
+}
---
+++ haoyi-service/src/test/java/com/haoyi/service/HaoyiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haoyi.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaoyiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
